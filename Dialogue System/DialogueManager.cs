4a6dcdb90adf25354235fe73127a543a1a3863bf
Pandadha <90336551+Pandadha@users.noreply.github.com>
2021-09-28 10:41:02 -0500

    commit2

---
+++ Dialogue System/DialogueManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI speakerName, dialogue, navButtonText;
+    public Image speakerSprite;
+
+    private int currentIndex;
+    private Conversation currentConvo;
+    private static DialogueManager instance;
+    private Animator anim;
+    private Coroutine typingCo;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            anim = GetComponent<Animator>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void StartConversation(Conversation convo)
+    {
+        instance.anim.SetBool("isOpen", true);
+        instance.currentIndex = 0;
+        instance.currentConvo = convo;
+        instance.speakerName.text = "";
+        instance.dialogue.text = "";
+        instance.navButtonText.text = "v";
+
+        instance.ReadNext();
+    }
+
+    public void ReadNext()
+    {
+        if(currentIndex > currentConvo.GetLength())
+        {
+            instance.anim.SetBool("isOpen", false);
+            return;
+        }
+        speakerName.text = currentConvo.GetLineByIndex(currentIndex).speaker.GetName();
+
+        if(typingCo == null)
+        {
+            typingCo = instance.StartCoroutine(TypeText(currentConvo.GetLineByIndex(currentIndex).dialogue));//dialogue.text = currentConvo.GetLineByIndex(currentIndex).dialogue;
+        }
+        else
+        {
+            instance.StopCoroutine(typingCo);
+            typingCo = null;
+            typingCo = instance.StartCoroutine(TypeText(currentConvo.GetLineByIndex(currentIndex).dialogue));
+        }
+        
+        speakerSprite.sprite = currentConvo.GetLineByIndex(currentIndex).speaker.GetSprite();
+        currentIndex++;
+
+        if(currentIndex > currentConvo.GetLength())
+        {
+            navButtonText.text = "x";
+        }
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        dialogue.text = "";
+        bool complete = false;
+        int index = 0;
+
+        while(!complete)
+        {
+            dialogue.text += text[index];
+            index++;
+            yield return new WaitForSeconds(0.02f);
+
+            if(index == text.Length)
+            {
+                complete = true;
+            }
+        }
+
+        typingCo = null;
+    }
+}
---
+++ Dialogue System/Tester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+
+ public Conversation[] convo;
+    private void Start()
+    {
+        StartConvo();
+    }
+    public void StartConvo() //start
+    {
+        DialogueManager.StartConversation(convo[0]);
+    }
+    public void Convo2()// found weapon
+    {
+        DialogueManager.StartConversation(convo[1]);
+    }
+    public void Convo3() // doctor mushroom
+    {
+        DialogueManager.StartConversation(convo[2]);
+    }
+    public void Convo4()
+    {
+        DialogueManager.StartConversation(convo[3]);
+    }
+}
